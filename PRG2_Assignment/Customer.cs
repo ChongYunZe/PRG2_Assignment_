333da7f55455c639436d01987d33278e2c0338f2
Daniel <s10258472@connect.np.edu.sg>
2024-01-11 20:58:37 +0800

    implemented customer, order and pointcard classes (not fully)

--- PRG2_Assignment/Customer.cs
+++ PRG2_Assignment/Customer.cs
@@ -11,5 +11,43 @@ namespace PRG2_Assignment
         private string name;
         public string Name { get;set; }
 
+        private int memberid;
+        public int Memberid { get; set; }
+
+        private DateTime dob;
+        public DateTime Dob { get; set; }
+
+        private List<Order> orderHistory;
+        public List<Order> OrderHistory { get; set;}
+
+        private PointCard rewards;
+
+        public PointCard Rewards { get; set; }
+
+        public Customer() { }
+
+        public Customer(string name, int memberid, DateTime dob)
+        {
+            Name = name;
+            Memberid = memberid;
+            Dob = dob;
+        }
+
+        public Order MakeOrder()
+        {
+
+        }
+
+        public bool IsBirthday()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name} Memberid: {Memberid} Dob {Dob}";
+        }
+
+
     }
 }
--- PRG2_Assignment/Order.cs
+++ PRG2_Assignment/Order.cs
@@ -8,5 +8,56 @@ namespace PRG2_Assignment
 {
     internal class Order
     {
+        private int id;
+
+        public int Id { get; set; }
+
+        private DateTime timeReceived;
+
+        public DateTime TimeReceived { get; set; }
+
+        private DateTime? timeFulfilled;
+
+        public DateTime? TimeFulfilled { get; set; }
+
+        private List<IceCream> iceCreamList;
+
+        public List<IceCream> IceCreamList { get; set; }
+
+        public Order()
+        {
+
+        }
+
+        public Order(int id, DateTime timeReceived)
+        {
+            Id=id;
+            TimeReceived=timeReceived;
+        }
+
+        public void ModifyIceCream(int id)
+        {
+
+        }
+
+        public void AddIceCream(IceCream)
+        {
+
+        }
+
+        public void DeleteIceCream(int id)
+        {
+
+        }
+
+        public double CalculateTotal()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return $"Id: {Id} Time Received: {TimeReceived} Time Fullfilled: {TimeFulfilled}";
+        }
     }
 }
